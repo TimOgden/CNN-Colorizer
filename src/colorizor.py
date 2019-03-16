782643eeb7fb35c9f38f81f507f11734df7d255e
Tim Ogden <togden1999@gmail.com>
2019-03-16 10:31:28 -0400

    Changes to U-net

--- src/colorizor.py
+++ src/colorizor.py
@@ -1,8 +1,7 @@
 from keras.datasets import cifar10
-from keras.layers import Conv2D, UpSampling2D, Flatten, Dense, MaxPooling2D
+from keras.layers import *
 from keras.optimizers import Adam
-from keras.layers import LeakyReLU, Dropout, BatchNormalization
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.callbacks import ModelCheckpoint, TensorBoard
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,6 +22,60 @@ def plot_rand_imgs():
 	for i in range(10):
 		plt.imshow(x_train[i+int(rand)])
 		plt.show()
+
+
+def build_unet(pretrained_weights=None, input_size=(32,32,1)):
+	inputs = Input(input_size)
+	conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+	conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+	conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+	conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+	conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+	conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+	pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+	conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+	conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+	drop4 = Dropout(0.5)(conv4)
+	pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+	conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+	conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+	drop5 = Dropout(0.5)(conv5)
+
+	up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+	merge6 = concatenate([drop4,up6], axis = 3)
+	conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+	conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+	up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+	merge7 = concatenate([conv3,up7], axis = 3)
+	conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+	conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+	up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+	merge8 = concatenate([conv2,up8], axis = 3)
+	conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+	conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+	up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+	merge9 = concatenate([conv1,up9], axis = 3)
+	conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+	conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+	conv9 = Conv2D(3, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+	conv10 = Conv2D(3, 1, activation = 'sigmoid')(conv9)
+
+	model = Model(input = inputs, output = conv10)
+
+	model.compile(optimizer = Adam(lr = 1e-4), loss = 'mean_absolute_error', metrics = ['accuracy'])
+	
+	#model.summary()
+
+	if(pretrained_weights):
+		model.load_weights(pretrained_weights)
+
+	return model
 
 def build_model():
 	dropout = .2
@@ -93,25 +146,28 @@ def generator(dataset):
 			gray_x = np.reshape(gray_x, (-1, gray_x.shape[0], gray_x.shape[1]))
 			gray_x = np.expand_dims(gray_x, axis=3)
 			x = np.reshape(x, (-1, x.shape[0], x.shape[1], x.shape[2]))
-			yield ({'conv2d_1_input': gray_x}, {'up_sampling2d_4': x})
+			yield ({'input_1': gray_x/255.}, {'conv2d_24': x/255.})
 
 def stepsOf(val):
 	return ceil(len(val)/batch_size)
 
-model = build_resnet()
-model.compile(optimizer=keras.optimizers.Adam(lr=.001), loss='mean_absolute_error', metrics=['accuracy'])
-print(model.summary())
 
-#model.fit(x=x_train/255., y=y_train/255., batch_size=batch_size, epochs=10, verbose=1,
-#	shuffle=True, validation_data=[x_test/255., y_test/255.])
 
-save_best = ModelCheckpoint('../weights/best.h5', monitor='val_loss', save_best_only=True, save_weights_only=True, verbose=1, mode='min')
-checkpoint = ModelCheckpoint('../weights/chkpt_{epoch:04d}.h5', monitor='val_loss', save_best_only=False, verbose=1, mode='min', period=10)
-tensorboard = TensorBoard(log_dir='../logs/{}'.format(time.time()), batch_size=64)
+if __name__=='__main__':
+	model = build_unet()
+	#model.compile(optimizer=keras.optimizers.Adam(lr=.001), loss='mean_absolute_error', metrics=['accuracy'])
+	print(model.summary())
 
-model.fit_generator(generator(x_train), steps_per_epoch=stepsOf(x_train), epochs=500, shuffle=True,
-	validation_data=generator(x_test), validation_steps=stepsOf(x_test), callbacks=[save_best, checkpoint, tensorboard])
-model.save('../weights/colorizor.h5')
+	#model.fit(x=x_train/255., y=y_train/255., batch_size=batch_size, epochs=10, verbose=1,
+	#	shuffle=True, validation_data=[x_test/255., y_test/255.])
 
-acc = model.evaluate(x_test)
-print(acc[1])
+	save_best = ModelCheckpoint('../weights/best.h5', monitor='val_loss', save_best_only=True, save_weights_only=True, verbose=1, mode='min')
+	checkpoint = ModelCheckpoint('../weights/chkpt_{epoch:04d}.h5', monitor='val_loss', save_best_only=False, verbose=1, mode='min', period=10)
+	tensorboard = TensorBoard(log_dir='../logs/{}'.format(time.time()), batch_size=64)
+
+	model.fit_generator(generator(x_train), steps_per_epoch=stepsOf(x_train), epochs=500, shuffle=True,
+		validation_data=generator(x_test), validation_steps=stepsOf(x_test), callbacks=[save_best, checkpoint, tensorboard])
+	model.save('../weights/colorizor.h5')
+
+	acc = model.evaluate(x_test)
+	print(acc[1])
--- src/colorizor_tester.py
+++ src/colorizor_tester.py
@@ -1,13 +1,65 @@
 import matplotlib.pyplot as plt
 from keras.datasets import cifar10
-from keras.models import Sequential
-from keras.layers import Conv2D, UpSampling2D, Flatten, Dense, MaxPooling2D
+from keras.models import Sequential, Model
+from keras.layers import *
 from keras.optimizers import Adam
-from keras.layers import LeakyReLU, Dropout, BatchNormalization
 import numpy as np
 import cv2
 
 (_,_), (x_test,_) = cifar10.load_data()
+
+def build_unet(pretrained_weights=None, input_size=(32,32,1)):
+	inputs = Input(input_size)
+	conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+	conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+	conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+	conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+	conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+	conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+	pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+	conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+	conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+	drop4 = Dropout(0.5)(conv4)
+	pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+	conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+	conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+	drop5 = Dropout(0.5)(conv5)
+
+	up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+	merge6 = concatenate([drop4,up6], axis = 3)
+	conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+	conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+	up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+	merge7 = concatenate([conv3,up7], axis = 3)
+	conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+	conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+	up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+	merge8 = concatenate([conv2,up8], axis = 3)
+	conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+	conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+	up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+	merge9 = concatenate([conv1,up9], axis = 3)
+	conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+	conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+	conv9 = Conv2D(3, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+	conv10 = Conv2D(3, 1, activation = 'sigmoid')(conv9)
+
+	model = Model(input = inputs, output = conv10)
+
+	model.compile(optimizer = Adam(lr = 1e-4), loss = 'mean_absolute_error', metrics = ['accuracy'])
+	
+	#model.summary()
+
+	if(pretrained_weights):
+		model.load_weights(pretrained_weights)
+
+	return model
 
 def build_model():
 	dropout = .2
@@ -60,28 +112,31 @@ def build_resnet():
 	])
 	return model
 
-model = build_resnet()
-model.load_weights('../weights/best.h5')
+model = build_unet(pretrained_weights= '../weights/best.h5')
+#model = build_model()
+
+
 img = x_test[10]
-normed_img = img / 255.
 plt.subplot(1,3,1)
 plt.title('Original Image')
 plt.imshow(img)
 
 gray_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+normed_gray = gray_x / 255.
 
 plt.subplot(1,3,2)
 plt.title('Grayscale Image')
 plt.imshow(gray_x, cmap='gray')
 print(gray_x.shape)
 
-gray_x = np.reshape(gray_x, (-1, gray_x.shape[0], gray_x.shape[1], 1))
-print(gray_x.shape)
+normed_gray = np.reshape(normed_gray, (-1, gray_x.shape[0], gray_x.shape[1], 1))
+print(normed_gray.shape)
 
-recolored = model.predict(gray_x)
+recolored = model.predict(normed_gray)*255.
 print(recolored[0][0][0])
 print(recolored.shape)
 recolored = np.reshape(recolored, (recolored.shape[1], recolored.shape[2], recolored.shape[3]))
+print(recolored.shape)
 #recolored *= 255.
 plt.subplot(1,3,3)
 plt.title('CNN Prediction')
